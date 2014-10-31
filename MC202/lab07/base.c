5b22443c6a6d66fe3eee9f31d3ca95bdba167203
Lauro Cruz <laurocruzsouza@gmail.com>
2014-10-31 20:17:02 -0200

    Submetido

--- MC202/lab07/base.c
+++ MC202/lab07/base.c
@@ -3,7 +3,7 @@
  * MC202 - Turma F                                                      *
  * laurocruzsouza@gmail.com / lauro.souza@students.ic.unicamp.br        *
  * Laboratório 07 - Base de dados com árvores de busca simples - base.c *
- * Last modified: 29-11-14                                              *
+ * Last modified: 31-10-14                                              *
  ************************************************************************/
 
 /*
@@ -20,31 +20,37 @@ typedef struct AuxNoArv {
 } NoArv, * ImplBase;
   
 int maiorValor(int a, int b) {
-	if (a > b)
-		return a;
+/* Retorna o maior valor inteiro */
+	if (a > b) return a;
 	else return b;
 }
 
-Aluno removeFolha (ImplBase *p) {
-	Aluno a;
+String removeFolha (ImplBase *p, int *ra) {
+/* Função que remove o aluno em um nó com dois filhos */
+	String s;
 	ImplBase out;
-	
+
+	/* Encontra o menor nó de menor ra da subárvore */
 	while ((*p)->esq != NULL)
-		p = &((*p)->esq);
+		p = &((*p)->esq); 
+
+	/* Armazena o ponteiro da string do nome do aluno que será retornado */
+	s = ((*p)->info).nome;
+	/* Troca o valor do RA no nó que era pra ser originalmente retirado */
+	*ra = ((*p)->info).ra;	
+
 	
-	a = (*p)->info;
 	out = *p;
-	
+	/* Altera o apontador do nó pai */
 	*p = (*p)->dir;
-	FREE((out->info).nome);
+	/* Libera a memória alocada pela nó */
 	FREE(out);
 
-	return a;
+	return s;
 }
 
 Base CriaBase() {
 /* Devolve apontador para uma base vazia */
-	
 	return NULL;
 }
 
@@ -53,22 +59,26 @@ Boolean InsereBase(Base *p, Aluno a) {
    com o mesmo valor de 'ra', e devolve 'true';  caso
    contrário devolve 'false' */
 	Boolean entrou = false;
-	ImplBase* b = (ImplBase*) p;
+	ImplBase* b = (ImplBase*) p; /* Casting da Base para ImplBase */
 
+	/* Se não entrar nem uma vez, *b é nulo e a base é vazia */
 	while (*b != NULL) {
 		entrou = true;
+		/* Percorre a árvore de acordo com as propriedades de árvore de busca */
 		if (a.ra < ((*b)->info).ra)
 			b = &((*b)->esq);
 		else if (a.ra > ((*b)->info).ra)
 			b = &((*b)->dir);
-		else return false;
+		else return false; /* Se encontro ra, o aluno já esxiste e não é possível inserí-lo */
 	}
 	
+	/* Cria o novo nó com as informações do aluno */
 	*b = MALLOC(sizeof(NoArv));
 	
 	(*b)->info = a;
 	(*b)->esq = (*b)->dir = NULL;
 	
+	/* Se a árvore for vazia, é preciso passar para o apontador da raiz o nó raiz */
 	if (!entrou)
 		*p = *b;
 
@@ -80,50 +90,65 @@ Boolean ConsultaBase(Base *p, int ra, Aluno *a) {
 /* Devolve 'true' se existe um registro com 'ra' dado na
    base 'p';  caso contrário devolve 'false'. 'a' conterá 
    os dados do aluno, se encontrado. */
-	ImplBase b = (ImplBase) *p;
+	ImplBase b = (ImplBase) *p; /* Casting de Base para ImplBase */
 
 	while (b != NULL) {
+		/* Percorre a árvore de acordo com as propriedades de árvore de busca */
 		if (ra < (b->info).ra)
 			b = b->esq;
 		else if (ra > (b->info).ra)
 			b = b->dir;
 		else {
+		/* Se encontra o RA, o aluno existe na árvore e suas informações armazenadas */
 			*a = b->info;
 			return true;
 		}
 	}
-
-	return false; /* PROVISÓRIO */
+	
+	/* Não encontrou o aluno */
+	return false; 
   
 } /* ConsultaBase */
 
 Boolean RemoveBase(Base *p, int ra) {
 /* Devolve 'true' se um registro com 'ra' pôde ser removido da
    base 'p';  caso contrário devolve 'false'. */
-	ImplBase* b = (ImplBase*) p;
+	ImplBase* b = (ImplBase*) p; /* Casting de *Base para *ImplBase */
 	ImplBase aux;
 	
 	while (*b != NULL) {
+		/* Percorre a árvore de acordo com as propriedades de árvore de busca */
 		if (ra < ((*b)->info).ra)
 			b = &((*b)->esq);
 		else if (ra > ((*b)->info).ra)
 			b = &((*b)->dir);
-		else {
+		else { /* Encontra o RA */	
+			/* Se pelo menos uma das subárvores do nó for nula é possível removê-lo */
 			if ((*b)->dir == NULL || (*b)->esq == NULL) {
 				aux = *b;
-				
+			
+				/* Se a subárvore direita for nula, o pai passa a aponta para a subárvore esquerda */
 				if ((*b)->dir == NULL) *b = (*b)->esq;
+				/* Se a subárvore esquerda for nula, o pai passa a apontar para a subárvore direita*/
 				else *b = (*b)->dir;
 				
+				/* Liberando o vetor do nome do aluno e o nó da árvore */
 				FREE((aux->info).nome);
 				FREE(aux);
 				
-			} else (*b)->info = removeFolha(&((*b)->dir));
-			
+			/* O nó a ser tirado apresenta ambas as subárvores */
+			} else { 
+				/* Libera o vetor do nome do aluno a ser removido */
+				FREE(((*b)->info).nome);
+				/* Retorna o apomtador para o nome do novo aluno e altera o valor do ra do nó */
+				((*b)->info).nome = removeFolha(&((*b)->dir), &(((*b)->info).ra));
+			}
+			/* Foi possível realizar a remoção */
 			return true;
 		}
 	}
 
+	/* O aluno a se remover não existe na árvore */
 	return false;
 
 } /* RemoveBase */
@@ -131,34 +156,41 @@ Boolean RemoveBase(Base *p, int ra) {
 int AlturaBase(Base *p) {
 /* Devolve a altura da base 'p'. */
 	int hEsq, hDir;
-	ImplBase* b = (ImplBase*) p;
+	ImplBase* b = (ImplBase*) p; /* Casting de *Base para *ImplBase */
 	Base* es, di;
 	
+	/* Quando a árvore for nula, sua altura é 0 */
 	if (*b == NULL)
 		return 0;
-	
+
+	/* Casting de *ImplBase para *Base */
 	es = (Base*) &((*b)->esq);
 	di = (Base*) &((*b)->dir);
 	
+	/* Obtém a altura das subárvores esquerda e direita */
 	hEsq = AlturaBase(es);
 	hDir = AlturaBase(di);
 	
+	/* A altura da árvore é a altura da maior subárvore mais 1 (nó raiz) */
 	return maiorValor(hEsq, hDir) + 1;
 
 } /* AlturaBase */
 
 int NumeroNosBase(Base *p) {
 /* Devolve o número de nós da base 'p'. */
-	ImplBase* b = (ImplBase*) p;
+	ImplBase* b = (ImplBase*) p; /* Casting de *Base para *ImplBase */
 	Base* es, di;
 	
+	/* Se a árvore for nula, ela não tem nenhum nó */
 	if (*b == NULL)
 		return 0;
 	
+	/* Casting de *ImplBase para *Base */
 	es = (Base*) &((*b)->esq);
 	di = (Base*) &((*b)->dir);
 	
-	
+	/* A quantidade de nós da árvore é igual a quantidade de nós das subárvores esquerda e direita
+	 * mais 1 (nó raiz) */
 	return NumeroNosBase(es) + NumeroNosBase(di) + 1;
 
 } /* NumeroNosBase */
@@ -167,15 +199,16 @@ int NumeroNosBase(Base *p) {
 void PercorreBase(Base *p, void (*Visita)(Aluno*)) {
 /* Executa um percurso inordem na base, invocando a função Visita
    para todos os elementos. */
-	ImplBase b = (ImplBase) *p;
+	ImplBase b = (ImplBase) *p; /* Casting de Base para ImplBase */
 	Base* es, in;
 	
 	while (b != NULL) {
+		/* Casting de *ImplBase para *Base */
 		es = (Base*) &(b->esq);
 		in = (Base*) &(b->info);
-		PercorreBase(es, Visita);
-		Visita(in);
-		b = b->dir;
+		PercorreBase(es, Visita); /* Percorre árvore esquerda */
+		Visita(in); /* Visita as informações do nó */
+		b = b->dir; /* Percorre árvore direita */
 	}
 
 }   /* PercorreBase */
@@ -183,16 +216,17 @@ void PercorreBase(Base *p, void (*Visita)(Aluno*)) {
 void LiberaBase(Base *p) {
 /* Libera todos os nós da base apontada por 'p', bem 
    como todas as cadeias que guardam os nomes. */
-	ImplBase b = (ImplBase) *p;
+	ImplBase b = (ImplBase) *p; /* Casting de Base para ImplBase */
 	Base* es, di;
 	
 	if (b != NULL) {
+		/* Casting de *ImplBase para *Base */
 		es = (Base*) &(b->esq);
 		di = (Base*) &(b->dir);
-		LiberaBase(es);
-		LiberaBase(di);
-		FREE((b->info).nome);
-		FREE(b);
+		LiberaBase(es); /* Libera subárvore esquerda */
+		LiberaBase(di); /* Libera subárvores esquerda */
+		FREE((b->info).nome); /* Libera string do nome do aluno */
+		FREE(b); /* Libera nó */
 	}
 
 } /* LiberaBase */

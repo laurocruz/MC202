f3ede85139cde7214a427e6302619c7c18648f50
Lauro Cruz <laurocruzsouza@gmail.com>
2014-12-02 18:21:20 -0200

    Submetido

--- MC202/lab11/base.c
+++ MC202/lab11/base.c
@@ -32,26 +32,20 @@ int Espalha(int k) {
     return (k%MaxHash);
 } /* Espalha */
 
-/* FUNCOES AUXILIARES */
-/* ------------------ */
-
-
-/* COMPLETAR SE NECESSARIO */
-
 
 /* FUNCOES DA INTERFACE */
 /* -------------------- */
 
 Base CriaBase() {
 /* Devolve apontador para uma base vazia com tabela inicializada */
-    ImplBase b = MALLOC(sizeof(RegBase));
+    ImplBase base = MALLOC(sizeof(RegBase));
 
-    b->numregs = 0;
+    base->numregs = 0;
 
     for (int i = 0; i < MaxHash; i++) {
-        b->tabela[i] = NULL;
+        base->tabela[i] = NULL;
     }
-    return (Base) b;
+    return (Base) base;
 } /* CriaBase */
 
 Boolean InsereBase(Base p, Aluno *a) {
@@ -59,29 +53,39 @@ Boolean InsereBase(Base p, Aluno *a) {
    com o mesmo valor de 'ra', e devolve 'true';  caso
    contrario devolve 'false' */
 
-    ImplBase b = p;
+    ImplBase base = p;
     int hash = Espalha((*a).ra);
-    Lista aux = b->tabela[hash];
+    Lista aux = base->tabela[hash];
 
     if (aux == NULL || (aux->aluno).ra > (*a).ra) {
-        b->tabela[hash] = MALLOC(sizeof(RegLista));
-        (b->tabela[hash])->prox = aux;
-        (b->tabela[hash])->aluno = *a;
+    /* Se a lista for vazia ou o ra do aluno a ser inserido for menor que o ra do primeiro
+     * aluno da lista, o aluno Ã© inserido na primira posicao */
+        base->tabela[hash] = MALLOC(sizeof(RegLista));
+        (base->tabela[hash])->prox = aux;
+        (base->tabela[hash])->aluno = *a;
+        (base->numregs)++;
         return true;
     } else {
         while (true) {
+            /* Caso encontre aluno com o mesmo RA, nao sera possivel inserir o aluno na tabela */
             if ((aux->aluno).ra == (*a).ra)
                 return false;
 
             if ((aux->aluno).ra < (*a).ra) {
+            /* Caso o RA do aluno apontado por aux seja menos que o RA do aluno a ser inserido, este
+             * sera inserido apos o aluno apontado por aux */
                 if ((aux->prox == NULL) || (((aux->prox)->aluno).ra > (*a).ra)) {
+                /* Caso nao tenha mais nenhum aluno na lista ou o proximo aluno tiver o RA maior, o
+                 * aluno sera inserido em frente ao aluno apontado por aux */
                     Lista aux1 = aux->prox;
                     aux->prox = MALLOC(sizeof(RegLista));
                     aux = aux->prox;
                     aux->prox = aux1;
                     aux->aluno = *a;
+                    (base->numregs)++;
                     return true;
                 } else aux = aux->prox;
+                /* Caso contrario, segue-se na lista */
             }
         }
     } 
@@ -91,28 +95,36 @@ Boolean RemoveBase(Base p, int ra) {
 /* Remove da base 'p' o aluno com 'ra' dado; devolve 'true'
    se ele existiu e 'false' caso contrario. */
     
-    ImplBase b = p;
+    ImplBase base = p;
     Lista aux;
     int hash = Espalha(ra);
 
-    if (b->tabela[hash] == NULL) return false;
+    /* Se a lista for vazia, nao e possivel remover o aluno */
+    if (base->tabela[hash] == NULL) return false;
 
-    if (((b->tabela[hash])->aluno).ra == ra) {
-        aux = (b->tabela[hash])->prox;
-        FREE(((b->tabela[hash])->aluno).nome);
-        FREE(b->tabela[hash]);
-        b->tabela[hash] = aux;
+
+    if (((base->tabela[hash])->aluno).ra == ra) {
+    /* Caso o aluno da primeira posicao da lista tenha o RA procurado */
+        aux = (base->tabela[hash])->prox;
+        FREE(((base->tabela[hash])->aluno).nome);
+        FREE(base->tabela[hash]);
+        base->tabela[hash] = aux;
+        (base->numregs)--;
         return true;
     } else {
-        aux = b->tabela[hash];
+        aux = base->tabela[hash];
         while (aux->prox != NULL) {
             if (((aux->prox)->aluno).ra == ra) {
+            /* Caso o aluno em frente ao aluno apontado por aux tenha o RA procurado, ele e
+             * removido */
                 Lista aux1 = aux->prox->prox;
                 FREE(((aux->prox)->aluno).nome);
                 FREE(aux->prox);
                 aux->prox = aux1;
+                (base->numregs)--;
                 return true;
             } else aux = aux->prox;
+            /* Caso contrario, segue-se em frente na lista */
         }
         return false;
     }
@@ -123,35 +135,28 @@ Boolean ConsultaBase(Base p, int ra, Aluno *a) {
 /* Devolve 'true' se existe um registro com 'ra' dado na
    base 'p';  caso contrario devolve 'false'.  'a' contera 
    os dados do aluno, se encontrado. */
-    ImplBase b = p;
+    ImplBase base = p;
     int hash = Espalha(ra); 
-    Lista aux = b->tabela[hash];
+    Lista aux = base->tabela[hash];
 
     while (aux != NULL) {
         if ((aux->aluno).ra == ra) {
+            /* Encontra o aluno com o RA procurado */
             (*a).nome = (aux->aluno).nome;
             (*a).ra = (aux->aluno).ra;
             return true;
         }
         aux = aux->prox;
     }
+    /* Nao encontra o aluno */
     return false;
 } /* ConsultaBase */
 
 int NumeroRegsBase(Base p) {
 /* Devolve o numero de registros (alunos) na base 'p'. */
-    ImplBase b = p;
-    Lista aux = NULL;
-    int i, n = 0;
+    ImplBase base = p;
 
-    for (i = 0; i < MaxHash; i++) {
-        aux = b->tabela[i];
-        while (aux != NULL)  {
-            n++;
-            aux = aux->prox;
-        }
-    }
-    return n;
+    return base->numregs;
 } /* NumeroRegsBase */
 
 void ImprimeBase(Base p) {
@@ -160,12 +165,16 @@ void ImprimeBase(Base p) {
    entrada, a ordem da lista ligada.  Cada linha começa com o índice
    correspondente na tabela de espalhamento. Deve ser usado o formato
    "(%3d) %06d %s\n". */
-    ImplBase b = p;
+    ImplBase base = p;
     Lista aux;
+
+    if (base->numregs == 0) return;
     
     for (int i = 0; i < MaxHash; i++) {
-        aux = b->tabela[i];
+    /* Percorre todas as listas da tabela */
+        aux = base->tabela[i];
         while (aux != NULL) {
+        /* Percorre os nos das listas */
             printf("(%3d) %06d %s\n", i, (aux->aluno).ra, (aux->aluno).nome);
             aux = aux->prox;
         }
@@ -176,19 +185,21 @@ void ImprimeBase(Base p) {
 void LiberaBase(Base p) {
 /* Libera toda a memoria utilizada pela base 'p', bem como
    todas as cadeias que guardam os nomes. */
-    ImplBase b = p;
+    ImplBase base = p;
     Lista aux1, aux2;
 
     for (int i = 0; i < MaxHash; i++) {
-        aux1 = b->tabela[i];
+    /* Percorre todas as listas da tabela */
+        aux1 = base->tabela[i];
         while (aux1 != NULL) {
+        /* Percorre os nos das listas */
             aux2 = aux1->prox;
             FREE((aux1->aluno).nome);
             FREE(aux1);
             aux1 = aux2;
         }
     }
-    FREE(b);
+    FREE(base);
 
 } /* LiberaBase */
 
